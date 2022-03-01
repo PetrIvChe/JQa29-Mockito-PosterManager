e499bc48ec10558697d66f912eda967298baef12
PetrIvChe <cherniavskiipetr@gmail.com>
2022-03-01 22:40:46 +0300

    Covered is 83%

---
+++ src/main/java/ru/netology/domain/Movies.java
@@ -0,0 +1,16 @@
+package ru.netology.domain;
+
+class Movies {
+
+    private int id;
+    private String title;
+    private String genre;
+
+
+    public Movies(int id, String title, String genre) {
+        this.id = id;
+        this.title = title;
+        this.genre = genre;
+
+    }
+}
